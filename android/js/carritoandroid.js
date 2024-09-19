0301e0dec1389503569cece70d08512234340209
jarcvon0663 <55066370+jarcvon0663@users.noreply.github.com>
2024-09-19 12:45:03 -0500

    Subida de proyecto

---
+++ android/js/carritoandroid.js
@@ -0,0 +1,276 @@
+const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+const botonComprar = document.querySelector("#carrito-acciones-comprar");
+const total = document.querySelector("#total");
+
+cargarProductosCarrito();
+
+function cargarProductosCarrito() {
+    if (productosEnCarrito.length > 0) {
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+        contenedorCarritoProductos.innerHTML = "";
+        
+        let mensajeDescuento = "";
+
+        productosEnCarrito.forEach((producto, index) => {
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="carrito-producto-titulo">
+                    <small>Título</small>
+                    <h3>${producto.titulo}</h3>
+                </div>
+                <div class="carrito-producto-precio">
+                    <small>Precio</small>
+                    <p>$${producto.precio.toLocaleString('es-CO')}</p>
+                </div>
+                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+            `;
+            contenedorCarritoProductos.append(div);
+
+            // Agregar mensajes de descuento según la cantidad de juegos en el carrito
+            if (productosEnCarrito.length === 1 && index === 0) {
+                mensajeDescuento = "¡Agrega 2 juegos más y obtén un descuento especial!";
+            } else if (productosEnCarrito.length === 2 && index === 1) {
+                mensajeDescuento = "¡Agrega 1 juego más y obtén un descuento especial!";
+            } else if (productosEnCarrito.length === 3 && index === 2) {
+                mensajeDescuento = "¡Agrega 2 juegos más y tendrás acceso a todo el catálogo!";
+            } else if (productosEnCarrito.length === 4 && index === 3) {
+                mensajeDescuento = "¡Agrega 1 juego más y tendrás acceso a todo el catálogo!";
+            } else if (productosEnCarrito.length === 5 && index === 4) {
+                mensajeDescuento = "¡Por una compra de $30.000 obtienes acceso a todo el catálogo!";
+            } else if (productosEnCarrito.length === 6 && index === 5) {
+                mensajeDescuento = "¡Por una compra de $30.000 obtienes acceso a todo el catálogo!";
+            } else if (productosEnCarrito.length === 7 && index === 6) {
+                mensajeDescuento = "¡Por una compra de $30.000 obtienes acceso a todo el catálogo!";
+            } else if (productosEnCarrito.length === 8 && index === 7) {
+                mensajeDescuento = "¡Por una compra de $30.000 obtienes acceso a todo el catálogo!";
+            }            
+
+            // Mostrar el mensaje de descuento si existe
+            if (mensajeDescuento !== "") {
+                const mensajeDescuentoElement = document.createElement("p");
+                mensajeDescuentoElement.textContent = mensajeDescuento;
+                contenedorCarritoProductos.append(mensajeDescuentoElement);
+            }
+        });
+        
+        actualizarBotonesEliminar();
+        actualizarTotal();
+    } else {
+        contenedorCarritoVacio.classList.remove("disabled");
+        contenedorCarritoProductos.classList.add("disabled");
+        contenedorCarritoAcciones.classList.add("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    }
+}
+
+
+function actualizarBotonesEliminar() {
+    const botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
+}
+
+function eliminarDelCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    if (index !== -1) {
+        productosEnCarrito.splice(index, 1);
+        localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+        cargarProductosCarrito();
+        Toastify({
+            text: "Juego eliminado",
+            duration: 3000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #4b33a8, #785ce9)",
+                borderRadius: "2rem",
+                textTransform: "uppercase",
+                fontSize: ".75rem"
+            }
+        }).showToast();
+    }
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+function vaciarCarrito() {
+    Swal.fire({
+        title: '¿Estás seguro?',
+        icon: 'question',
+        html: `Se van a borrar ${productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0)} juegos.`,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: 'Sí',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            productosEnCarrito.length = 0;
+            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+            cargarProductosCarrito();
+        }
+    });
+}
+
+function mostrarMetodoPago() {
+    var metodoSeleccionado = document.getElementById("metodo-pago").value;
+
+    if (!metodoSeleccionado) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor selecciona un método de pago'
+        });
+        return;
+    }
+
+    // Obtener el total a pagar desde el DOM
+    var totalPagar = document.getElementById("total").textContent;
+
+    // Configuración para cada método de pago
+    var infoPago = {
+        nequi: {
+            titulo: "3143394021",
+            texto: `a nombre de Jei*** R***<br><br><b>Total a pagar: ${totalPagar}</b>`,
+            link: "nequi://app",
+            img: "../img/nequi.webp"
+        },
+        daviplata: {
+            titulo: "3143394021",
+            texto: `a nombre de Jei*** R***<br><br><b>Total a pagar: ${totalPagar}</b>`,
+            link: "https://play.google.com/store/apps/details?id=com.davivienda.daviplataapp",
+            img: "../img/daviplata.webp"
+        },
+        dale: {
+            titulo: "3143394021",
+            texto: `a nombre de Jei*** R***<br><br><b>Total a pagar: ${totalPagar}</b>`,
+            link: "https://play.google.com/store/apps/details?id=com.avalsolucionesdigitalessa.dale_app_embedded",
+            img: "../img/dale.webp"
+        },
+        transfiya: {
+            titulo: "3143394021",
+            texto: `Desde cualquier aplicación bancaria paga por transfiya al Nequi 3143394021\nTotal a pagar: ${totalPagar}`,
+            img: "../img/transfiya.webp"
+        },
+        pse: {
+            titulo: "Realiza una recarga desde cualquier entidad bancaria a Nequi 3143394021",
+            texto: `Total a pagar: ${totalPagar}`,
+            link: "https://recarga.nequi.com.co/bdigitalpsl/?_ga=2.189700267.1416431342.1700047997",
+            img: "../img/pse.webp"
+        },
+        bancolombia: {
+            titulo: "Bancolombia o Ahorro a la mano cuenta de ahorros: 30400010907",
+            texto: `Total a pagar: ${totalPagar}`,
+            link: "https://play.google.com/store/apps/details?id=co.com.tcs.bancolombia.bancaalamano",
+            img: "../img/bancolombia.webp"
+        }
+    };
+
+    var metodo = infoPago[metodoSeleccionado];
+    
+    Swal.fire({
+        title: metodo.titulo,
+        html: metodo.texto, // Cambiar de 'text' a 'html' para interpretar etiquetas HTML
+        imageUrl: metodo.img,
+        imageWidth: 100,
+        imageHeight: 100,
+        confirmButtonText: 'Abrir la APP',
+        showCancelButton: true,
+        cancelButtonText: 'Cerrar',
+        background: '#ececec', // Fondo gris claro
+        color: 'var(--clr-main)', // Asegúrate de que el texto sea visible
+        imageAlt: metodo.titulo // Alternativo de imagen para accesibilidad
+    }).then((result) => {
+        if (result.isConfirmed && metodo.link) {
+            window.open(metodo.link, "_blank");
+        }
+    });
+    
+}
+
+
+botonComprar.addEventListener("click", comprarCarrito);
+function comprarCarrito() {
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    cargarProductosCarrito();
+    Toastify({
+        text: "Gracias por tu compra",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #4b33a8, #785ce9)",
+            borderRadius: "2rem",
+            textTransform: "uppercase",
+            fontSize: ".75rem"
+        }
+    }).showToast();
+}
+
+function actualizarTotal() {
+    let totalCalculado = 0;
+    let descuento = 0;
+
+    productosEnCarrito.forEach(producto => {
+        totalCalculado += producto.precio; // Sumar el precio de cada juego al total inicialmente
+    });
+
+    // Aplicar descuentos según la cantidad de juegos en el carrito
+    if (productosEnCarrito.length >= 3 && productosEnCarrito.length < 5) {
+        descuento = 3000; // Descuento para 3 juegos: se descuentan $3000
+    } else if (productosEnCarrito.length >= 5) {
+        totalCalculado = 30000; // Descuento para todos los juegos: total $30000
+    }
+
+    // Si se agregan 5 juegos, se aplica el descuento especial
+    if (productosEnCarrito.length === 5) {
+        totalCalculado = 30000; // Descuento para todos los juegos: total $30000
+    } else {
+        // Aplicar descuento al total calculado si hay descuento
+        totalCalculado -= descuento;
+    }
+
+    // Actualizar el texto del total en el HTML
+    total.innerText = `$${totalCalculado.toLocaleString('es-CO')}`;
+
+    // Actualizar el texto del descuento en el HTML
+    const descuentoElement = document.getElementById("descuento");
+    if (descuento > 0) {
+        descuentoElement.textContent = `Descuento: -$${descuento.toLocaleString('es-CO')}`;
+    } else {
+        descuentoElement.textContent = "";
+    }
+}
+
+function actualizarMensaje(event) {
+    // Evitar que se abra la página por defecto
+    event.preventDefault();
+
+    // Obtener los títulos de los videojuegos en el carrito
+    const carrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
+    const titulos = carrito.map(producto => producto.titulo);
+
+    // Crear el mensaje con los títulos de los videojuegos
+    const mensaje = encodeURIComponent(`Hola, te envío el comprobante de la consignación para instalar en mi celular los siguientes videojuegos: ${titulos.join(', ')}`);
+
+    // Actualizar el href del enlace del botón de WhatsApp con el mensaje generado
+    document.getElementById('whatsapp-link').href = `https://api.whatsapp.com/send?phone=573042672810&text=${mensaje}`;
+
+    // Seguir con el enlace después de actualizar el href
+    window.location = document.getElementById('whatsapp-link').href;
+}
---
+++ js/main.js
@@ -0,0 +1,286 @@
+let productos = [];
+
+fetch("./js/productos.json")
+  .then((response) => response.json())
+  .then((data) => {
+    productos = data;
+    cargarProductos(productos);
+  })
+  .catch((error) => console.error("Error al cargar los productos:", error));
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar;
+const numerito = document.querySelector("#numerito");
+const numeritoFlotante = document.querySelector("#numerito-flotante");
+
+botonesCategorias.forEach((boton) =>
+  boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+  })
+);
+
+function cargarProductos(productosElegidos) {
+  contenedorProductos.innerHTML = "";
+  productosElegidos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">Precio $${producto.precio.toLocaleString('es-CO')}</p>
+                <a class="producto-trailer" href="${producto.trailer}" target="_blank">
+                <p class="video-trailer">
+                <span class="emoji">🎬</span>Vídeo
+            </p>
+                </a>
+                <p class="producto-advertencia"><span class="emoji">⚠️</span>${producto.advertencia}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+    contenedorProductos.append(div);
+  });
+
+  actualizarBotonesAgregar();
+
+  const trailerLinks = document.querySelectorAll(".producto-trailer");
+  trailerLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      mostrarTrailer(this.href);
+    });
+  });
+
+  const popupClose = document.querySelector(".popup-close");
+  popupClose.addEventListener("click", cerrarTrailer);
+}
+
+function mostrarTrailer(url) {
+  const videoId = getYoutubeVideoId(url);
+  const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+  const popup = document.getElementById("popup-trailer");
+  const popupVideo = document.getElementById("popup-video");
+  popupVideo.src = embedUrl;
+  popup.style.display = "block";
+}
+
+function cerrarTrailer() {
+  const popup = document.getElementById("popup-trailer");
+  const popupVideo = document.getElementById("popup-video");
+  popup.style.display = "none";
+  popupVideo.src = "";
+}
+
+function getYoutubeVideoId(url) {
+  const urlObj = new URL(url);
+  return urlObj.hostname === "youtu.be"
+    ? urlObj.pathname.slice(1)
+    : urlObj.searchParams.get("v");
+}
+
+window.addEventListener("click", function (event) {
+  const popup = document.getElementById("popup-trailer");
+  if (event.target == popup) {
+    cerrarTrailer();
+  }
+});
+
+botonesCategorias.forEach((boton) => {
+  boton.addEventListener("click", (e) => {
+    botonesCategorias.forEach((boton) => boton.classList.remove("active"));
+    e.currentTarget.classList.add("active");
+
+    const categoriaId = e.currentTarget.id;
+    if (categoriaId != "todos") {
+      const productosFiltrados = productos.filter(
+        (producto) => producto.categoria.id === categoriaId
+      );
+      const productoCategoria = productosFiltrados[0];
+      tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+      cargarProductos(productosFiltrados);
+    } else {
+      tituloPrincipal.innerText = "Todos los productos";
+      cargarProductos(productos);
+    }
+  });
+});
+
+function actualizarBotonesAgregar() {
+  botonesAgregar = document.querySelectorAll(".producto-agregar");
+  botonesAgregar.forEach((boton) =>
+    boton.addEventListener("click", agregarAlCarrito)
+  );
+}
+
+let productosEnCarrito =
+  JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
+actualizarNumerito();
+
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const productoAgregado = productos.find(
+    (producto) => producto.id === idBoton
+  );
+
+  // Verificar si el producto ya está en el carrito
+  const productoExistente = productosEnCarrito.find(
+    (producto) => producto.id === idBoton
+  );
+
+  // Crear el contenido HTML para la notificación
+  const contenido = `
+        <div style="display: flex; align-items: center; padding: 5px; width: 100vw; max-width: 300px; margin: 10px auto;">
+        <img src="${productoAgregado.imagen}" alt="Imagen del juego" style="width: 70px; height: 70px; margin-right: 5px;" />
+        <div style="flex: 1;">
+            <p style="margin: 0; font-size: 14px; color: #ffffff;">${productoAgregado.titulo} ${productoExistente ? "ya está en el carrito" : "se agregó al carrito"}</p>
+            <div style="margin-top: 10px;">
+                <button id="btnHacerPago" style="background-color: #00641e; color: white; padding: 8px 12px; border: none; border-radius: 5px; cursor: pointer; margin-right: 5px;">Pagar</button>
+                <button id="btnSeguirEscogiendo" style="background-color: #ff0000; color: white; padding: 8px 12px; border: none; border-radius: 5px; cursor: pointer;">Añadir +</button>
+            </div>
+        </div>
+    </div>
+    `;
+
+  // Mostrar la notificación
+  const toast = Toastify({
+    node: (() => {
+      const div = document.createElement("div");
+      div.innerHTML = contenido;
+      return div;
+    })(),
+    gravity: "top", // Posición superior
+    position: "center", // En la derecha
+    duration: 7000, // Duración más larga
+    close: false, // Ocultar la "X"
+    stopOnFocus: true, // Evitar que se cierre al pasar el ratón sobre la notificación
+    style: {
+      background: "linear-gradient(to right, #4b33a8, #785ce9)",
+      borderRadius: "2rem",
+      fontSize: ".85rem",
+    },
+  }).showToast();
+
+  // Asignar los eventos a los botones después de mostrar la notificación
+  setTimeout(() => {
+    document.getElementById("btnHacerPago").onclick = () => {
+      document.getElementById("hacerpago").click(); // Simular clic en "Hacer pago"
+    };
+
+    document.getElementById("btnSeguirEscogiendo").onclick = () => {
+      toast.hideToast(); // Cerrar la notificación
+      Toastify({
+        text: "¡Sigue explorando!",
+        duration: 2000,
+        gravity: "top",
+        position: "right",
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          fontSize: ".85rem",
+        },
+      }).showToast();
+    };
+  }, 100); // Esperar un momento para asegurarse de que los botones se carguen
+
+  if (!productoExistente) {
+    // Agregar el producto al carrito si no existe
+    productoAgregado.cantidad = 1;
+    productosEnCarrito.push(productoAgregado);
+    localStorage.setItem(
+      "productos-en-carrito",
+      JSON.stringify(productosEnCarrito)
+    );
+
+    animarProductoAlCarrito(e.currentTarget); // Ejecutar la animación de agregar al carrito
+  }
+
+  actualizarNumerito(); // Actualizar el numerito del carrito
+}
+
+function agitarBotonPago() {
+  const botonPago = document.querySelector(".boton-flotante");
+  botonPago.classList.add("agitar");
+  setTimeout(() => {
+    botonPago.classList.remove("agitar");
+  }, 500); // Ajustar el tiempo según sea necesario
+}
+
+// Función de animación de agregar al carrito
+function animarProductoAlCarrito(imagen) {
+  const carrito = document.querySelector(".boton-flotante");
+  const imagenRect = imagen.getBoundingClientRect();
+  const carritoRect = carrito.getBoundingClientRect();
+
+  const imagenClon = imagen.cloneNode(true);
+  imagenClon.classList.add("animate-to-cart");
+  document.body.appendChild(imagenClon);
+
+  const scrollY = window.scrollY; // Obtener la posición de desplazamiento actual
+
+  imagenClon.style.top = `${imagenRect.top + scrollY}px`; // Ajustar por el desplazamiento
+  imagenClon.style.left = `${imagenRect.left}px`;
+  imagenClon.style.width = `${imagenRect.width}px`;
+  imagenClon.style.height = `${imagenRect.height}px`;
+
+  requestAnimationFrame(() => {
+    imagenClon.style.transform = `translate(${
+      carritoRect.left - imagenRect.left
+    }px, ${carritoRect.top - imagenRect.top}px) scale(0.1)`;
+    imagenClon.style.width = `10px`;
+    imagenClon.style.height = `10px`;
+  });
+
+  setTimeout(() => {
+    imagenClon.remove();
+  }, 2000);
+}
+
+document.addEventListener("DOMContentLoaded", actualizarNumerito);
+
+function actualizarNumerito() {
+  const nuevoNumerito = productosEnCarrito.reduce(
+    (acc, producto) => acc + producto.cantidad,
+    0
+  );
+  numerito.innerText = nuevoNumerito;
+  numeritoFlotante.innerText = nuevoNumerito; // Actualizar también el numerito del botón flotante
+}
+
+function buscarJuegos() {
+  const input = document.getElementById("search-input");
+  const term = input.value.toLowerCase();
+  const juegosFiltrados = productos.filter((juego) =>
+    juego.titulo.toLowerCase().includes(term)
+  );
+  cargarProductos(juegosFiltrados);
+}
+
+function ordenarPorPopularidad() {
+  const juegosOrdenados = [...productos].sort((a, b) =>
+    a.id.localeCompare(b.id)
+  );
+  cargarProductos(juegosOrdenados);
+}
+
+function ordenarAlfabeticamente() {
+  const juegosOrdenados = [...productos].sort((a, b) =>
+    a.titulo.localeCompare(b.titulo)
+  );
+  cargarProductos(juegosOrdenados);
+}
+
+function ordenarPorIdAlta() {
+  const juegosOrdenados = productos.filter(
+    (producto) => producto.categoria.id === "PCalta"
+  );
+  cargarProductos(juegosOrdenados);
+}
+
+function ordenarPorIdBaja() {
+  const juegosOrdenados = productos.filter(
+    (producto) => producto.categoria.id === "PCbaja"
+  );
+  cargarProductos(juegosOrdenados);
+}
